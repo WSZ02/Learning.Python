f5eedb557f3ae6af78f9d9e15485090e0b4352b3
Wojtek Szymanek <w.szymanek02@gmail.com>
2024-01-12 23:20:07 +0100

    adding grid and colors to random generated boxes

--- MemoryGame/main.py
+++ MemoryGame/main.py
@@ -3,6 +3,44 @@ from flet import *
 import time
 import random
 
+
+
+class GenerateGrid(UserControl):
+    def __init__(self):
+        super().__init__()
+        self.grid = Column(opacity=1, animate_opacity=300)
+        super().__init__()
+
+    def build(self):
+        rows: list = [
+            Row(
+                alignment=MainAxisAlignment.CENTER,
+                controls=[
+                    Container(
+                        width=54,
+                        height=54,
+                        animate=300,
+                        border=border.all(1, "white"),
+                        on_click=None,  #change later
+                    )
+                    for _ in range(5)
+                ],
+            )
+            for _ in range(5)
+        ]
+
+        colors: list = ["#5c443b", "#4cbbb5"]
+
+        for row in rows:
+            for container in row.controls[:]:
+                container.bgcolor = random.choices(colors, weights=[10, 2])[0]
+
+        self.grid.controls = rows
+        return self.grid
+
+        pass
+
+
 def main(page: Page):
     page.horizontal_alignment = "center"
     page.vertical_alignment = "center"
@@ -36,6 +74,7 @@ def main(page: Page):
         Row(alignment=MainAxisAlignment.CENTER, controls=[result]),
         Divider(height=10, color="transparent"),
         Divider(height=10, color="transparent"),
+        GenerateGrid(),
         #Stage of the game row
         Row(alignment=MainAxisAlignment.CENTER, controls=[stage]),
         Divider(height=10, color="transparent"),
